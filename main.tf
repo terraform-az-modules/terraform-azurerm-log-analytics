7aefacf8f7d9038171812c7b3e9ae646c35df30b
karan-cd <karan.tolambiya@clouddrove.com>
2026-08-18 02:28:01 +0530

    fix: updated variables type and tfcheck

--- main.tf
+++ main.tf
@@ -18,16 +18,16 @@ module "labels" {
 ## Log Analytics Workspace – Deploy Log Analytics workspace with diagnostic monitoring
 ##------------------------------------------------------------------------------------
 resource "azurerm_log_analytics_workspace" "main" {
-  count                      = var.enabled ? 1 : 0
-  name                       = var.resource_position_prefix ? format("law-%s", local.name) : format("%s-law", local.name)
-  location                   = var.location
-  resource_group_name        = var.resource_group_name
-  sku                        = var.log_analytics_workspace_sku
-  retention_in_days          = var.retention_in_days
-  daily_quota_gb             = var.daily_quota_gb
-  internet_ingestion_enabled = var.internet_ingestion_enabled
-  internet_query_enabled     = var.internet_query_enabled
-  tags                       = module.labels.tags
+  count                          = var.enabled ? 1 : 0
+  name                           = var.resource_position_prefix ? format("law-%s", local.name) : format("%s-law", local.name)
+  location                       = var.location
+  resource_group_name            = var.resource_group_name
+  sku                            = var.log_analytics_workspace_sku
+  retention_in_days              = var.retention_in_days
+  daily_quota_gb                 = var.daily_quota_gb
+  internet_ingestion_access_type = var.internet_ingestion_access_type
+  internet_query_access_type     = var.internet_query_access_type
+  tags                           = module.labels.tags
 }
 
 ##-----------------------------------------------------------------------------
